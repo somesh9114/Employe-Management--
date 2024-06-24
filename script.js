0eeabdb5fd4bc157a22a9de703922cc28f96a390
somesh kumar mati <someshmati.rocky@gmail.com>
2024-06-24 13:16:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var typed = new Typed('#element', {
+    strings: ['Software Developer','Graphic Designer','Mentor', 'Educator.'],
+    typeSpeed: 70,
+  });
+
+
+const prevEle = document.querySelector(".prev")
+const nextEle = document.querySelector(".next")
+const imgContainerEle = document.querySelector(".img-container")
+const imgEle = document.querySelectorAll(".pr-img")
+
+let currImg = 1;
+let timeout;
+
+nextEle.addEventListener("click", ()=>{
+    currImg++
+    clearInterval(timeout)
+    updateImg()
+})
+prevEle.addEventListener("click", ()=>{
+    currImg--
+    clearTimeout(timeout)
+    updateImg()
+})
+
+updateImg()
+
+function updateImg(){
+    if(currImg > imgEle.length){
+        currImg = 1
+    }else if(currImg < 1){
+        currImg = imgEle.length
+    }
+    imgContainerEle.style.transform = `translateX(${-(currImg - 1) * 100}%)`
+
+    timeout = setTimeout(()=>{
+        currImg++
+        updateImg()
+    }, 2000)
+}
+
